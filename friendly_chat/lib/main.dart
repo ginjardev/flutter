08ab4f055868f9d202bf4c654734db71d35231ec
Iniubong Arthur <arthurshur@gmail.com>
2022-02-27 17:05:47 +0000

    chatScreen added

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -2,15 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
-      title: "FriendlyChat",
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('FriendlyChat'),
-        ),
-      ),
-    )
+     FriendlyChatApp()
   );
 }
 
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     title: "FriendlyChat",
+     home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+      ),
+    );
+  }
+}
