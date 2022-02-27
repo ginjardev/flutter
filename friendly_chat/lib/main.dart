cc76f34c73d4ae10736104b9d661c2b68fa481a1
Iniubong Arthur <arthurshur@gmail.com>
2022-02-27 16:16:35 +0000

    making friendly chat app

---
+++ friendly_chat/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      title: "FriendlyChat",
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('FriendlyChat'),
+        ),
+      ),
+    )
+  );
+}
+
