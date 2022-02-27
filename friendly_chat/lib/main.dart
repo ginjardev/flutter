082c2addc615c04553eb34f0cbbc19209107872e
Iniubong Arthur <arthurshur@gmail.com>
2022-02-27 18:16:38 +0000

    chat interface

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-     FriendlyChatApp()
-  );
+  runApp(FriendlyChatApp());
 }
 
 class FriendlyChatApp extends StatelessWidget {
@@ -14,20 +12,61 @@ class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     title: "FriendlyChat",
-     home: ChatScreen(),
+      title: "FriendlyChat",
+      home: ChatScreen(),
     );
   }
 }
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({ Key? key }) : super(key: key);
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('FriendlyChat'),
+        centerTitle: true,
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  void _handleSubmitted(String text){
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: const InputDecoration.collapsed(hintText: 'Send a message'),
+                    ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                onPressed: ()=> _handleSubmitted(_textController.text), 
+                icon: Icon(Icons.send),
+              ),
+            )
+          ]
+        ),
       ),
     );
   }
