417db3f10b550d4bf729c08987d6f9ecfac3dc4b
Iniubong Arthur <arthurshur@gmail.com>
2022-02-28 22:34:21 +0000

    chat message UI

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -39,34 +39,71 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  void _handleSubmitted(String text){
+  void _handleSubmitted(String text) {
     _textController.clear();
   }
 
   Widget _buildTextComposer() {
-
     return IconTheme(
       data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          children: [
-            Flexible(
-              child: TextField(
+        child: Row(children: [
+          Flexible(
+            child: TextField(
               controller: _textController,
               onSubmitted: _handleSubmitted,
-              decoration: const InputDecoration.collapsed(hintText: 'Send a message'),
-                    ),
+              decoration:
+                  const InputDecoration.collapsed(hintText: 'Send a message'),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
-                onPressed: ()=> _handleSubmitted(_textController.text), 
-                icon: Icon(Icons.send),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              onPressed: () => _handleSubmitted(_textController.text),
+              icon: Icon(Icons.send),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+  String _name = 'Your Name';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(_name[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.headline4,
               ),
-            )
-          ]
-        ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
