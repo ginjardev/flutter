6897d5138c8012f88b0ab9f02297e849988f6944
Iniubong Arthur <arthurshur@gmail.com>
2022-03-07 12:37:52 +0000

    slivers screen

---
+++ sylvas/lib/slivers.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SliversApp extends StatefulWidget {
+  const SliversApp({ Key? key }) : super(key: key);
+
+  @override
+  State<SliversApp> createState() => _SliversAppState();
+}
+
+class _SliversAppState extends State<SliversApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
