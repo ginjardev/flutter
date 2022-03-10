bec6d9d7b6e5852828260fb89ac810193ba0d793
Iniubong Arthur <arthurshur@gmail.com>
2022-03-10 04:18:41 +0000

    advanced widgets ui

---
+++ widgets_ui/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+  int _selectedIndex = 0;
+
+   void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          bottom: TabBar(controller: _tabController, tabs: const [
+            Tab(
+              icon: Icon(Icons.cloud_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.beach_access_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.brightness_5_sharp),
+            ),
+          ]),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            const Center(
+              child: Text("It's cloudy here"),
+            ),
+            const Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text("It's sunny here"),
+                  FlutterLogo(),
+                  Icon(Icons.sentiment_very_satisfied_rounded)
+                ],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_sharp), label: "home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm), label: "Alarm"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bluetooth_audio_outlined), label: "bluetooth")
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+    );
+  }
+}
