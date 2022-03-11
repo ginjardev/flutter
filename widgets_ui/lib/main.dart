b2d3ecfddfd2490f590052deb99f210cedcf82d7
Iniubong Arthur <arthurshur@gmail.com>
2022-03-11 03:29:04 +0000

    added drawer menu

--- widgets_ui/lib/main.dart
+++ widgets_ui/lib/main.dart
@@ -47,54 +47,112 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          bottom: TabBar(controller: _tabController, tabs: const [
-            Tab(
-              icon: Icon(Icons.cloud_outlined),
-            ),
-            Tab(
-              icon: Icon(Icons.beach_access_sharp),
-            ),
-            Tab(
-              icon: Icon(Icons.brightness_5_sharp),
-            ),
-          ]),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            const Center(
-              child: Text("It's cloudy here"),
-            ),
-            const Center(
-              child: Text("It's rainy here"),
-            ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Text("It's sunny here"),
-                  FlutterLogo(),
-                  Icon(Icons.sentiment_very_satisfied_rounded)
-                ],
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            bottom: TabBar(controller: _tabController, tabs: const [
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
               ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ]),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: const [
+                SizedBox(
+                  height: 80,
+                  child: DrawerHeader(
+                  child: Text(
+                    "Menu",
+                    style: TextStyle(
+                      fontSize: 32
+                    ),
+                    textAlign: TextAlign.left,
+                    ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.rectangle
+                  ),
+                ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.notifications
+                  ),
+                  title: Text("Notifications"),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.message
+                  ),
+                  title: Text("Message"),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.person_sharp
+                  ),
+                  title: Text("Profile"),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.settings
+                  ),
+                  title: Text("Settings"),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout
+                  ),
+                  title: Text("Logout"),
+                ),
+                // Divider(
+                //   thickness: 2,
+                //   color: Colors.black,
+                // )
+              ],
             ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_sharp), label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.access_alarm), label: "Alarm"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.bluetooth_audio_outlined), label: "bluetooth")
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+          ),
+          body: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              const Center(
+                child: Text("It's cloudy here"),
+              ),
+              const Center(
+                child: Text("It's rainy here"),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text("It's sunny here"),
+                    FlutterLogo(),
+                    Icon(Icons.sentiment_very_satisfied_rounded)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home_sharp), label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.access_alarm), label: "Alarm"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.bluetooth_audio_outlined), label: "bluetooth")
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
+      ),
     );
   }
 }
