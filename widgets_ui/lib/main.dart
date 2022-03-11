0951c1f922e17d65bd50a871306786f5ec7d9026
Iniubong Arthur <arthurshur@gmail.com>
2022-03-11 05:04:04 +0000

    dropdown Menu & popup menu

--- widgets_ui/lib/main.dart
+++ widgets_ui/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -28,17 +28,22 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
 
+var _selection = WhyFarther.harder;
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   int _selectedIndex = 0;
 
-   void _onItemTapped(int index) {
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
   late TabController _tabController;
+  String dropdownValue = "One";
+
   @override
   void initState() {
     super.initState();
@@ -49,109 +54,143 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-            bottom: TabBar(controller: _tabController, tabs: const [
-              Tab(
-                icon: Icon(Icons.cloud_outlined),
-              ),
-              Tab(
-                icon: Icon(Icons.beach_access_sharp),
-              ),
-              Tab(
-                icon: Icon(Icons.brightness_5_sharp),
-              ),
-            ]),
-          ),
-          drawer: Drawer(
-            child: ListView(
-              children: const [
-                SizedBox(
-                  height: 80,
-                  child: DrawerHeader(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            PopupMenuButton<WhyFarther>(
+              onSelected: (WhyFarther result) {
+                setState(() {
+                  _selection = result;
+                });
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<WhyFarther>>[
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.harder,
+                  child: Text('Working a lot harder'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.smarter,
+                  child: Text('Being a lot smarter'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.selfStarter,
+                  child: Text('Being a self-starter'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.tradingCharter,
+                  child: Text('Placed in charge of trading charter'),
+                ),
+              ],
+            )
+          ],
+          bottom: TabBar(controller: _tabController, tabs: [
+            Tab(
+              icon: Icon(Icons.cloud_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.beach_access_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.brightness_5_sharp),
+            ),
+          ]),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: const [
+              SizedBox(
+                height: 80,
+                child: DrawerHeader(
                   child: Text(
                     "Menu",
-                    style: TextStyle(
-                      fontSize: 32
-                    ),
+                    style: TextStyle(fontSize: 32, color: Colors.white),
                     textAlign: TextAlign.left,
-                    ),
+                  ),
                   decoration: BoxDecoration(
-                    color: Colors.blue,
-                    shape: BoxShape.rectangle
-                  ),
+                      color: Colors.green, shape: BoxShape.rectangle),
                 ),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.notifications
-                  ),
-                  title: Text("Notifications"),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.message
-                  ),
-                  title: Text("Messages"),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.person_sharp
-                  ),
-                  title: Text("Profile"),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.settings
-                  ),
-                  title: Text("Settings"),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.logout
-                  ),
-                  title: Text("Logout"),
-                ),
-                // Divider(
-                //   thickness: 2,
-                //   color: Colors.black,
-                // )
-              ],
+              ),
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text("Notifications"),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text("Messages"),
+              ),
+              ListTile(
+                leading: Icon(Icons.person_sharp),
+                title: Text("Profile"),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout"),
+              ),
+              // Divider(
+              //   thickness: 2,
+              //   color: Colors.black,
+              // )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: <Widget>[
-              const Center(
-                child: Text("It's cloudy here"),
+            Center(
+              child: 
+                  Text("It's rainy here"),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text("It's sunny here"),
+                  FlutterLogo(),
+                  Icon(Icons.sentiment_very_satisfied_rounded)
+                ],
               ),
-              const Center(
-                child: Text("It's rainy here"),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text("It's sunny here"),
-                    FlutterLogo(),
-                    Icon(Icons.sentiment_very_satisfied_rounded)
-                  ],
-                ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home_sharp), label: "home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.access_alarm), label: "Alarm"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.bluetooth_audio_outlined), label: "bluetooth")
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_sharp), label: "home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm), label: "Alarm"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bluetooth_audio_outlined), label: "bluetooth")
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
